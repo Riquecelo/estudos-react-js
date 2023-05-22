b56c496f9b1c4af1933c4ef197bc6649470537c5
Marcelo Santos <riquecelox462m@gmail.com>
2023-05-22 15:25:33 -0300

    iniciando com props

---
+++ my-app/src/components/props/PropsBasico.js
@@ -0,0 +1,11 @@
+function PropsBasico(props){
+
+    return(
+        <>
+            <h3>Usando Props</h3>
+            <p>Olá {props.nome}, a Props foi usada!</p>
+        </>
+    )
+}
+
+export default PropsBasico
