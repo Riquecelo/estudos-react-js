4919176c75aaf740c22dfbf478be25adec87604b
Marcelo Santos <marcelo.has@hotmail.com>
2023-06-25 22:42:28 -0300

    utilização do react-icons

--- my-app/src/components/layout/Footer.js
+++ my-app/src/components/layout/Footer.js
@@ -1,11 +1,17 @@
+//npm install react-icons
+import { SiVtex, SiReact, SiJavascript, SiNpm, SiGit} from "react-icons/si";
+import styles from './Footer.module.css'
+
 function Footer(){
     return(
-        <ul>
-            <li>lorem</li>
-            <li>lorem</li>
-            <li>lorem</li>
-            <li>lorem</li>
-            <li>lorem</li>
+
+        <ul className={styles.social_list}>
+            <p>Rodapé da página</p>
+            <li><SiVtex/></li>
+            <li><SiReact/></li>
+            <li><SiJavascript/></li>
+            <li><SiNpm/></li>
+            <li><SiGit/></li>
         </ul>
     )
 }
